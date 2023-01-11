5c0678d4a237dcd01929eb490cbc062195c1236f
fenggang <fenggang@chongho.net>
2023-01-11 16:54:44 +0800

    chore: assets public path fix

--- .dumirc.ts
+++ .dumirc.ts
@@ -6,4 +6,7 @@ export default defineConfig({
     name: 'vk-components',
   },
   plugins: ['@umijs/plugins/dist/antd'],
+  // 为了让github pages部署成功，先将publicPath设置为/vk-components/
+  // 后续如果要部署到其他地方，再改这里，先写死，保证github pages部署成功
+  publicPath: '/vk-components/',
 });
